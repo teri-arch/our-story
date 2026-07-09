423dfb5afb0b96b2c90eb511d940119d5caef0c6
Abhinav Raj <araj36276@gmail.com>
2026-07-10 01:12:24 +0530

    Added password protection

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
+import PasswordGate from "./PasswordGate";
 import Hero from "./components/Hero";
 import Letter from "./components/Letter";
 import Gallery from "./components/Gallery";
-import PasswordGate from "./PasswordGate";
+import Timeline from "./components/Timeline";
+import Promises from "./components/Promises";
+import Ending from "./components/Ending";
+import Footer from "./components/Footer";
+import MusicPlayer from "./components/MusicPlayer";
 
 export default function App() {
   return (
     <PasswordGate>
+      <MusicPlayer />
       <Hero />
       <Letter />
       <Gallery />
+      <Timeline />
+      <Promises />
+      <Ending />
+      <Footer />
     </PasswordGate>
   );
 }
